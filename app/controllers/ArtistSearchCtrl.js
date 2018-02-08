d79a0cc25909adc884c57e1deeef3d8ace30874f
Sarah Morris <sarah.morris9@gmail.com>
2018-02-08 11:38:57 -0600

    started brewerydb call

---
+++ app/controllers/ArtistSearchCtrl.js
@@ -0,0 +1,38 @@
+"use strict";
+const angular = require("angular");
+const ngRoute = require("angular-route");
+
+angular.module("BrewifyApp").controller("ArtistSearchCtrl", ArtistSearchCtrl);
+ArtistSearchCtrl.$inject = ["$scope", "Spotify", "Artist", "Pairings"];
+function ArtistSearchCtrl($scope, Spotify, Artist, Pairings) {
+  $scope.login = () => {
+    Spotify.login();
+  };
+
+  $scope.search = () => {
+    Spotify.search($scope.searchTerm, "artist").then(function(data) {
+      $scope.results = data.data.artists.items;
+      $scope.results = $scope.results.map(artist => {
+        return new Artist(artist);
+      });
+    });
+  };
+
+  $scope.clicked = genres => {
+    console.log("genres", genres);
+    Pairings.getPairings().then(beers => {
+      for (let i = 0; i < genres.length; i++) {
+        let currentGenre = genres[i];
+        if (beers[currentGenre] !== undefined) {
+          $scope.beer = beers[currentGenre];
+          break;
+        }
+      }
+    });
+  };
+}
+
+console.log("Welcome to Brewify!");
+console.log(
+  "Login first, then search for an artist to get a link to their Spotify page!"
+);
