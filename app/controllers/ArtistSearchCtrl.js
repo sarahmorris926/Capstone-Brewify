4174ca71fb242fa8b41e2a821b3f2de8e088128d
Sarah Morris <sarah.morris9@gmail.com>
2018-02-08 14:09:11 -0600

    starting to add sass?

--- app/controllers/ArtistSearchCtrl.js
+++ app/controllers/ArtistSearchCtrl.js
@@ -19,19 +19,19 @@ function ArtistSearchCtrl($scope, Spotify, Artist, Pairings) {
   };
 
   $scope.clicked = genres => {
-         Pairings.getPairings().then(beers => {
-           for (let i = 0; i < genres.length; i++) {
-             let currentGenre = genres[i];
-             if (beers[currentGenre] !== undefined) {
-               $scope.beer = beers[currentGenre];
-               break;
-             }
-           }
-         });
-       };
-     }
-     
-     console.log("Welcome to Brewify!");
-     console.log(
-       "Login first, then search for an artist to get a link to their Spotify page!"
-     );
+    Pairings.getPairings().then(beers => {
+      for (let i = 0; i < genres.length; i++) {
+        let currentGenre = genres[i];
+        if (beers[currentGenre] !== undefined) {
+          $scope.beer = beers[currentGenre];
+          break;
+        }
+      }
+    });
+  };
+}
+
+console.log("Welcome to Brewify!");
+console.log(
+  "Login first, then search for an artist to get a link to their Spotify page!"
+);
